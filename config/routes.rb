510d14cb35df4822ef9bcacf9e37d8930f80a6dd
TakahiroSono <taka.sono889@gmail.com>
2018-10-18 12:31:47 +0900

    chenge root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'static_page/testScss'
+  root 'static_page#testScss'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
